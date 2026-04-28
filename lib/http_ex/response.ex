866d2dec10498a0dc0dbf9caea7e76078f38a10d
Marcel Horlings <m.horlings@hey.com>
2026-04-28 09:36:47 +0200

    Inspect value for response body
    In some cases we don't get the response_body in honeycomb, this might be
    because the value isn't properly formatted. In any case adding this
    shouldn't cause issues even if it doesn't help, so we might as well
    check if this improves behaviour.

--- lib/http_ex/response.ex
+++ lib/http_ex/response.ex
@@ -55,7 +55,7 @@ defmodule HTTPEx.Response do
     Shared.trace_attrs([
       {"error", error?},
       {"http.error", reason},
-      {"http.response_body", response.body},
+      {"http.response_body", Shared.inspect_value(response.body)},
       {"http.response_headers", inspect(response.headers)},
       {"http.status_code", response.status},
       {"http.retries", response.retries}
